5a860473ee7b7c8144b26719af1fba9ac18aa221
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2023-11-13 18:00:50 +0600

    Changed name for rollCheck.js to roleCheck.js

--- Server/routes/test.js
+++ Server/routes/test.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const auth = require('../middleware/auth');
-const roleCheck = require('../middleware/rollcheck');
+const roleCheck = require('../middleware/rolecheck');
 
 router.get('/details',auth, roleCheck(['admin']), (req, res) => {
     res.status(200).send({ message: "User authotized" });
